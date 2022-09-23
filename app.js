a8198b015196891a15281ed3772ba31c9495f7d1
brewksin <sbrukson@gmail.com>
2022-09-22 22:23:55 -0400

    serve the html file

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var finalhandler = require('finalhandler')
 var http = require('http')
 var Router = require('router')
+var fs = require('fs')
 
 const port = 3000
 
 var router = Router()
 // routes
 var home = router.route('/')
-var newRoute = router.route('/testing123')
+var newRoute = router.route('/projects.html')
 
 home.get(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end('Hello World!')
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    fs.createReadStream('index.html').pipe(res)
 })
 
 newRoute.get(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
     res.end('Welcome to the Testing World!')
 })
 
