eb5c61d95ef67c4c246e2c1e3cb5c3d237233bfb
brewksin <sbrukson@gmail.com>
2022-11-09 22:11:12 -0500

    running on cloud

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# specify the node base image with your desired version node:<version>
-FROM node:18
-WORKDIR .
-COPY . .
-RUN ["npm", ""]
-CMD ["node", "app.js"]
-
-# replace this with your application's default port
-EXPOSE 3000
--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var http = require('http')
 var Router = require('router')
 var fs = require('fs')
 
-const hostname = 'localhost'
-const port = 3000
+const port = parseInt(process.env.PORT) || 8080;
 
 var router = Router()
 // routes
@@ -12,11 +11,11 @@ var home = router.route('/')
 var newRoute = router.route('/projects.html')
 
 
-function handleHome (req, res) {
-     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-// fs.createReadStream('index.html').pipe(res)
-     var str = "We're noobs at coding, but we're on our way up!"
-     let ranString = rollIt()
+function handleHome(req, res) {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    // fs.createReadStream('index.html').pipe(res)
+    var str = "We're noobs at coding, but we're on our way up!"
+    let ranString = rollIt()
     res.write(str)
 
     // const buf = Buffer.alloc(11, 'aGVsbG8gd29ybGQ=', 'base64')
@@ -25,15 +24,15 @@ function handleHome (req, res) {
     // res.write(buf, 'utf8', function() {
     //     console.log("Writing Buffer Data...")
     // });
-    
+
     // // Creating buffer
     // const buff = Buffer.from(' hello world', 'utf8')
-    
+
     // // Writing the buffer data.
     // res.write(buff, 'utf8', function () {
     //     console.log("Writing Buffer Data...")
     // })
-    
+
     // Prints Output on the browser in response
     res.write('<br></br>')
     res.write(ranString)
@@ -41,27 +40,22 @@ function handleHome (req, res) {
     res.end(' ok')
 }
 home.get(handleHome)
-
 newRoute.get(function (req, res) {
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
     res.end('Welcome to the Testing World!')
 })
 
+const strings = ['sup', 'hey', 'sgud', 'waguan', 'wuss gucci', 'how goes', 'whatup', 'wazzupppp', 'how r u', 'yalla'];
+function rollIt() {
+    let ranString = strings[Math.floor(Math.random() * 10)]
+    return ranString
+}
+
+
 var server = http.createServer(function (req, res) {
     router(req, res, finalhandler(req, res))
 })
 
- server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-}); 
-
-
-const strings = ['sup', 'hey', 'sgud', 'waguan', 'wuss gucci', 'how goes', 'whatup', 'wazzupppp', 'how r u', 'yalla'];
-
-function rollIt () {
-   let ranString = strings[Math.floor(Math.random() * 10)]
-   return ranString
-}
-
-
-
+server.listen(port, () => {
+    console.log(`Server running at ${port}`);
+});
