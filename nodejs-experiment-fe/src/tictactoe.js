1207d59ac8468409815d359db1e459794a3a5d56
brewksin <sbrukson@gmail.com>
2022-12-02 23:37:12 -0500

    history

--- nodejs-experiment-fe/src/tictactoe.js
+++ nodejs-experiment-fe/src/tictactoe.js
@@ -67,12 +67,13 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -83,13 +84,21 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const squares = current.squares.slice();
         const winner = calculateWinner(squares);
 
@@ -99,7 +108,7 @@ class Game extends React.Component {
                     'Go to move #' + move :
                     'Go to game start';
                 return (
-                    <li>
+                    <li key={move}>
                         <button onClick={() => this.jumpTo(move)}>
                             {desc}
                         </button>
