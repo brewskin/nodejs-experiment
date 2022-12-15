1d41f9b5186c4a0c77f2f1bd19b7cf90b5de4f3d
brewksin <sbrukson@gmail.com>
2022-12-14 21:56:31 -0500

    add link for excali

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejs-experiment
+
+[Design](https://excalidraw.com/#room=98ad82a44e5e5ee7d305,jVWi7NaVGQSaXDmgzSf6VA)
--- nodejs-experiment-fe/src/App.js
+++ nodejs-experiment-fe/src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Judgement extends React.Component {
+
+}
 
 function App() {
   return (
