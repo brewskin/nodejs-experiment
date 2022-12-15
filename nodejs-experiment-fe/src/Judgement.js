9c84254ccef65b08f45cfc15cc6a61ae962e92c0
brewksin <sbrukson@gmail.com>
2022-12-14 22:37:04 -0500

    judgement react

---
+++ nodejs-experiment-fe/src/Judgement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Judgement extends React.Component {
+  render() {
+    return (
+      <div>{this.props.value}</div>
+    );
+  }
+}
+
+
+export default Judgement;
--- nodejs-experiment-fe/src/index.js
+++ nodejs-experiment-fe/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Game from './tictactoe';
+import Judgement from './Judgement';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Game />
+    <Judgement
+      user='this fucking guy'
+      value='Argument Numero Uno'
+    />
   </React.StrictMode>
 );
 
